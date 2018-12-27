4a832792fb8373d2861a0a679bff9271fa600375
Simon Morgan <sjm@sjm.io>
2018-12-27 18:52:56 +0000

    Document Utilities.FilenameFromUri(Uri).

--- Utilities.cs
+++ Utilities.cs
@@ -2,6 +2,11 @@ namespace httpc
 {
     public static class Utilities
     {
+        /// <summary>
+        /// Retrieve the filename component from a URI.
+        /// </summary>
+        /// <param name="uri">A URI to a file.</param>
+        /// <returns>The filename from the specified URI.</returns>
         public static string FilenameFromUri(System.Uri uri)
         {
             return uri.Segments[uri.Segments.Length - 1];
