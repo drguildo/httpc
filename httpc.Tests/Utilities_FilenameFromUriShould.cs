a7db02ba5609dc09f4cb78c92e47dc9b6adbfc96
Simon Morgan <sjm@sjm.io>
2018-12-27 19:18:54 +0000

    Add an MSTest project

---
+++ httpc.Tests/Utilities_FilenameFromUriShould.cs
@@ -0,0 +1,13 @@
+namespace httpc.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Utilities_FilenameFromUriShould
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
